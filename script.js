bfc40843d02a38837a032cc9b9cde62360f173fe
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-01 15:50:39 +0200

    Added axios to generate only one mail

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: "#root",
     data: {
-
+        email: "",
+    },
+    methods: {
+    },
+    created() {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
+            console.log(response);
+            this.email = response.data.response;
+        });
     }
 });
